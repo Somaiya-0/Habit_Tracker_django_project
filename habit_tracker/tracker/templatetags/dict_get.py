1152c5d669fe8970cfc9d573344c46ff08e8de68
Somaiya-0 <sdark.babe@gmail.com>
2025-05-18 06:02:32 +0600

    Saving the checkboxes perfectly working

--- habit_tracker/tracker/templatetags/dict_get.py
+++ habit_tracker/tracker/templatetags/dict_get.py
@@ -1,6 +1,9 @@
 from django import template
+
 register = template.Library()
 
 @register.filter
 def dict_get(d, key):
-    return d.get(key)
+    if isinstance(d, dict):
+        return d.get(key, '')
+    return ''
